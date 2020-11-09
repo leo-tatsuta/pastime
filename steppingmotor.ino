e1ed14d1523453ba3b853f8f9a1dc0c8a045a3a4
Leo IIDA <50055234+leo-tatsuta@users.noreply.github.com>
2020-11-10 07:50:08 +0900

    ステッピングモータ回すコード追加
    ステッピングモータ回すやつ

---
+++ steppingmotor.ino
@@ -0,0 +1,36 @@
+const int DIR  = 5;
+const int STEP = 2;
+int pulse = 600;
+
+void setup() {
+  pinMode(DIR, OUTPUT);
+  pinMode(STEP, OUTPUT);
+  digitalWrite(DIR, LOW);
+  digitalWrite(STEP, LOW);
+}
+
+void loop() {
+  
+  digitalWrite(DIR, HIGH);
+
+  for (int i=0; i<200; i++) {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(pulse);
+  }
+
+  delay(1000);
+  
+  digitalWrite(DIR, LOW);
+  
+  for (int i=0; i<200; i++) {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(pulse);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(pulse);
+  }
+
+  delay(1000);
+
+}
